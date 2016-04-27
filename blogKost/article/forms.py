983757fa6c92d31f9b0d91c1d62549ee9545d93c
skylifewww <skylife@ukr.net>
2016-04-28 00:29:33 +0300

    add

--- blogKost/article/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms.models import ModelForm
-from article.models import Comments
-
-
-class CommentForm(ModelForm):
-
-    class Meta:
-        model = Comments
-        fields = ["comments_text"]
--- blogKost/loginsys/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-import loginsys.views
-
-urlpatterns = [
-    url(r'^register/', loginsys.views.register),
-    url(r'^login/', loginsys.views.login),
-    url(r'^logout/', loginsys.views.logout),
-
-]
