104391173463b600afafd3349f6edee891237717
skylifewww <skylife@ukr.net>
2016-04-28 04:41:08 +0300

    run

--- article/admin.py
+++ article/admin.py
@@ -11,7 +11,7 @@ class CommentsInLine(admin.StackedInline):
 
 
 class ArticleAdmin(AdminVideoMixin, admin.ModelAdmin):
-    fields = ["article_author", "article_title", "article_video", "article_date", 'short_text_ru', 'short_text_en',
+    fields = ["article_author", "article_title", "article_date", 'short_text_ru', 'short_text_en',
               'full_text_ru', 'full_text_en',
               "article_tag", "article_category", 'image']
     inlines = [CommentsInLine]
