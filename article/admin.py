3e542a15daed325025226f7a7699672884adf486
skylifewww <skylife@ukr.net>
2016-05-05 00:02:18 +0300

    redactor

--- article/admin.py
+++ article/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from article.models import *
 from embed_video.admin import AdminVideoMixin
 from redactor.widgets import RedactorEditor
+from django import forms
 
 
 # Register your models here.
@@ -11,16 +12,22 @@ class CommentsInLine(admin.StackedInline):
     extra = 0
 
 
-class ArticleAdmin(AdminVideoMixin, admin.ModelAdmin):
-    fields = ["article_author", "article_title", "article_video", "article_date", 'short_text_ru', 'short_text_en',
-              'full_text_ru', 'full_text_en',
-              "article_tag", "article_category", 'image']
-              widgets = {
+class ArticleAdminForm(forms.ModelForm):
+    class Meta:
+        model = Article
+         widgets = {
            'short_text_ru': RedactorEditor(),
            'short_text_en': RedactorEditor(),
            'full_text_ru': RedactorEditor(),
            'full_text_en': RedactorEditor(),
         }
+
+
+class ArticleAdmin(AdminVideoMixin, admin.ModelAdmin):
+    fields = ["article_author", "article_title", "article_video", "article_date", 'short_text_ru', 'short_text_en',
+              'full_text_ru', 'full_text_en',
+              "article_tag", "article_category", 'image']
+    form = EntryAdminForm
     inlines = [CommentsInLine]
     list_filter = ["article_date", "article_tag", "article_category", "article_author"]
     search_fields = ["article_title", "article_author", "article_category", "article_tag"]
